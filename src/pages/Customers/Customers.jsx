21539873c098e4ef7a4dba89918271d709fcaa35
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-10-17 21:17:24 -0300

    feat: create customers page

---
+++ src/pages/Customers/Customers.jsx
@@ -0,0 +1,39 @@
+import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { Title } from '../../components/Title/Title';
+import { User } from '@phosphor-icons/react';
+
+import './_Customers.scss';
+
+export const Customers = () => {
+    return (
+        <div className="customers">
+            <Sidebar />
+            <section className="customers-content">
+                <Title title="Clientes" icon={<User size={30} />} />
+
+                <div className="form-container">
+                    <form className="customers-form">
+                        <div className="customers-form-control">
+                            <label htmlFor="fantasyName">Nome da Empresa</label>
+                            <input
+                                type="text"
+                                name="fantasyName"
+                                placeholder="Nome da sua empresa"
+                            />
+                        </div>
+                        <div className="customers-form-control">
+                            <label htmlFor="cnpj">Nome da Empresa</label>
+                            <input type="text" name="cnpj" placeholder="Seu CNPJ" />
+                        </div>
+                        <div className="customers-form-control">
+                            <label htmlFor="address">Endereço</label>
+                            <input type="text" name="address" placeholder="Nome da sua empresa" />
+                        </div>
+
+                        <button type="submit">Cadastrar</button>
+                    </form>
+                </div>
+            </section>
+        </div>
+    );
+};
