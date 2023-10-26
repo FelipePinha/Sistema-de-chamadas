b77bd6b0c8ed48594ac5cf800813fa1c206b0c10
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-10-26 14:25:53 -0300

    chore: add query client

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 import './styles/app.scss';
+
+const queryClient = new QueryClient();
 
 const App = () => {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <Outlet />
-        </>
+        </QueryClientProvider>
     );
 };
 
